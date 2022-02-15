76be16da599242dcb13697a1bd4e17059243d228
Dirk Metzler <metzler@bio.lmu.de>
2022-02-15 15:10:05 +0000

    version 3.2.2

--- inst/doc/jaatha-evolution.R
+++ inst/doc/jaatha-evolution.R
@@ -1,13 +1,16 @@
-## ------------------------------------------------------------------------
-library(coala)
-model <- coal_model(c(10, 15), 100) +
-  feat_mutation(par_range("theta", 1, 10)) +
-  feat_migration(par_range("m", 0, 3), symmetric = TRUE) +
-  feat_pop_merge(par_range("t_split", 0.1, 2), 2, 1) + 
-  feat_recombination(1) +
-  sumstat_jsfs()
+## -----------------------------------------------------------------------------
+if(require("coala")) {	
+   model <- coal_model(c(10, 15), 100) +
+      feat_mutation(par_range("theta", 1, 10)) +
+      feat_migration(par_range("m", 0, 3), symmetric = TRUE) +
+      feat_pop_merge(par_range("t_split", 0.1, 2), 2, 1) + 
+      feat_recombination(1) +
+      sumstat_jsfs()
+}	
 
-## ------------------------------------------------------------------------
-library(jaatha)
-jaatha_model <- create_jaatha_model(model)
+## -----------------------------------------------------------------------------
+if(requireNamespace("coala")) {
+     library(jaatha)
+     jaatha_model <- create_jaatha_model(model)
+}
 
